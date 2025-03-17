d00520decdad13ce0a39e9ab8998ee33e6184a2c
Miguel <miguelangelpatinobuiles@gmail.com>
2025-03-16 22:15:49 -0500

    Add files via upload

---
+++ arreglos multidimensionales 1/arreglo2.js
@@ -0,0 +1,18 @@
+let arreglo = [ [ ["%", 7, true, "#"] ], [ [false, 0, 99, "?"] ], [ [44, 55, 66, 77] ] ]
+
+console.log(arreglo[1][0][3]);
+console.log(arreglo[2][0][2]);
+console.log(arreglo[0][0][2]);
+console.log(arreglo[0][0][0]);
+
+
+// Complete sin usar el intérprete de Javascript(úselo al final para verificar sus respuestas):
+// e) arreglo[0][0][2] devolverá __true__
+// f) arreglo[2][0][0] devolverá __44__
+// g) arreglo[1][0][3] devolverá __?__
+// h) arreglo[0][1][1] devolverá ___error___
+
+console.log(arreglo[0][0][2]);
+console.log(arreglo[2][0][0]);
+console.log(arreglo[1][0][3]);
+// console.log(arreglo[0][1][1]);  aqui hay error pero no se bien porque, queda esa duda pendiente
---
+++ arreglos multidimensionales 1/arreglo4.js
@@ -0,0 +1,15 @@
+let arreglo = [ [50, 60, 70], ["q", "%", "$"], ["?", ">", "<"], [1 , -8, -2] ]
+
+arreglo [1][1] = "?";
+console.log(arreglo[1][1]);
+
+arreglo [3][1] = "9";
+console.log(arreglo[3][1]);
+
+arreglo [0][0] = "¡";
+console.log(arreglo[0][0]);
+
+// arreglo [1][2].splice (1, 3);
+// console.log(arreglo[1][2]);  ME DA ERROR, NO SE PORQUE NO ME DEJA ELIMINAR
+
+
